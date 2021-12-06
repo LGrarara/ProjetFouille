ccccaf7ed170cefd03c91158cf2e432dc61696ff
Amaury <amaurylange@gmail.com>
2021-12-06 10:54:38 +0100

    Back to basic

--- src/main.c
+++ src/main.c
@@ -7,12 +7,11 @@
 
 typedef struct Point
 {
-	char nom[100];
 	float Courage;
 	float Loyaute;
 	float Sagesse;
 	float Malice;
-	char maison[100];
+	int cluster;
 } Point;
 typedef Point P_Point;
 
@@ -20,7 +19,6 @@ typedef struct Cluster
 {
 	int taille;
 	int centre;
-	P_Point C_Cluster[];
 } Cluster;
 typedef Cluster P_Cluster;
 
@@ -31,146 +29,176 @@ float getDistance(Point a, Point b)
 	return s;
 }
 
-int minimum(int i, int j)
+void initialisePoints(P_Point points[], int n)
 {
-	int m = (i < j) ? i : j;
-	return m;
-}
-
-//retourne le max de 2 valeur
-int maximum(int i, int j)
-{
-	int m = (i > j) ? i : j;
-	return m;
-}
-
-void parsenPoint(int initalnumber, P_Point init[], FILE *fp)
-{
-
 	int i;
-	int j = 0;
-	char line[150];
 
-	fgets(line, 150, fp);
+	char *file_path = "choixpeauMagique.csv";
+	FILE *fp = fopen(file_path, "r");
 
-	while (fgets(line, 150, fp))
+	if (!fp)
+		printf("Can't open file\n");
+
+	else
 	{
-		char *str = strdup(line);
 
-		int init_size = strlen(str);
+		char buffer[1024];
 
-		char *ptr = strtok(str, ";");
-		int i = 1;
+		int row = 0;
+		int column = 0;
 
-		while (ptr != NULL)
+		while (fgets(buffer,
+					 1024, fp))
 		{
-			if (i == 1)
-				strcpy(init[j].nom, ptr);
+			column = 0;
+			row++;
 
-			if (i == 2)
-				init[j].Courage = atof(ptr);
+			if (row == 1)
+				continue;
 
-			if (i == 3)
-				init[j].Loyaute = atof(ptr);
+			char *value = strtok(buffer, ";");
 
-			if (i == 4)
-				init[j].Sagesse = atof(ptr);
+			while (value)
+			{
 
-			if (i == 5)
-				init[j].Malice = atof(ptr);
+				// Column 2
+				if (column == 1)
+				{
 
-			if (i == 6)
-				init[j].Courage = atof(ptr);
+					points[row - 1].Courage = atof(value);
+				}
 
-			if (i == 1)
-				strcpy(init[j].maison, ptr);
+				// Column 3
+				if (column == 2)
+				{
 
-			ptr = strtok(NULL, ";");
-			i++;
+					points[row - 1].Loyaute = atof(value);
+				}
+
+				// Column 4
+				if (column == 3)
+				{
+
+					points[row - 1].Sagesse = atof(value);
+				}
+
+				// Column 5
+				if (column == 4)
+				{
+					points[row - 1].Malice = atof(value);
+				}
+
+				value = strtok(NULL, ";");
+				column++;
+			}
 		}
-		j++;
-	}
-}
 
-int initialnumberobject(FILE *fp)
-{
-	int ch = 0;
-	int lines = 0;
-
-	while (!feof(fp))
-	{
-		ch = fgetc(fp);
-		if (ch == '\n')
-		{
-			lines++;
-		}
-	}
-	rewind(fp);
-	return lines - 1;
-}
-void numercluster(int K, int initialnumber, P_Cluster clustertab[K], P_Point init[50])
-{
-	srand(time(0));
-	int i;
-	for (i = 0; i < K; i++)
-		clustertab[i].taille = 0;
-
-	for (i = 0; i < K; i++)
-	{
-		clustertab[i].centre = rand() % initialnumber + 1;
-		P_Point buffer = init[clustertab[i].centre];
-
-		clustertab[i].C_Cluster[0] = buffer;
-
-		// printf("%f\n", clustertab[0].C_Cluster[0].Loyaute);
+		// Close the file
+		fclose(fp);
 	}
 }
 
 void initialiseDistance(float distance[50][50], P_Point points[], int n)
 {
+
 	int i, j;
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i + 1; j++)
+		for (j = 0; j < n; j++)
 		{
 			distance[i][j] = getDistance(points[i], points[j]);
-			if (i == 0 && j == 8)
-				printf("%f", distance[0][8]);
 		}
 	}
 }
-void creecluster(int intab[], P_Cluster clustertab[], int initialnumber, P_Point init[50], int K)
+
+void initialisecluster(int k, int n, P_Cluster clusters[], P_Point points[])
 {
+
+	srand(time(0));
+	int i;
+	for (i = 0; i < k; i++)
+		clusters[i].taille = 0;
+
+	for (i = 0; i < k; i++)
+	{
+		clusters[i].centre = rand() % n + 1;
+		points[clusters[i].centre].cluster = 0;
+		clusters[i].taille++;
+	}
+}
+
+void afficheCluster(P_Cluster clusters[], int k)
+{
+	for (int i = 0; i < k; i++)
+	{
+		printf("centre: %d\n", clusters[i].centre);
+		printf("Taille: %d\n", clusters[i].taille);
+		printf("suivant\n");
+	}
+}
+
+int find_minimum(float a[], int k)
+{
+	int c, index = 0;
+
+	float min;
+
+	for (c = 1; c < k; c++)
+		if (a[c] < min)
+			index = c;
+
+	return index;
+}
+
+void affecter_cluster_le_plus_proche(int k, int n, P_Cluster clusters[], P_Point points[])
+{
+	//objet i de 0 à n, chercher le cluster le plus proche de de n, i++
+	int i, j, e;
+	float tmp;
+	float minimum = 900000000;
+
+	float minimumtab[k];
+
+	for (e = 0; e < n; e++)
+	{
+		for (i = 0; i < k; i++)
+			minimumtab[i] = minimum;
+
+		for (j = 0; j < k; j++)
+		{
+			for (i = 0; i < n; i++)
+			{
+				tmp = getDistance(points[i], points[clusters[j].centre]);
+				if (tmp < minimumtab[j])
+					minimumtab[j] = tmp;
+			}
+			if (minimumtab[j] == 0)
+			{
+				points[e].cluster = j;
+				clusters[j].taille++;
+				break;
+			}
+			else
+			{
+				points[e].cluster = find_minimum(minimumtab, k);
+				clusters[find_minimum(minimumtab, k)].taille++;
+			}
+		}
+	}
 }
 
 int main()
 {
-	FILE *fp = fopen("choixpeauMagique.csv", "r");
-	if (fp == NULL)
-	{
-		printf("Erreur de lecture");
-		exit(EXIT_FAILURE);
-	}
+	int n = 50; //points
+	int k = 6;	//groupes
+	if (k > n)
+		exit(0);
 
-	int initialnumber = initialnumberobject(fp);
-	P_Point init[initialnumber];
-	rewind(fp);
-	// Prend le fichier en parametre et convertit en tableau de point
-	parsenPoint(initialnumber, init, fp);
-	int K = 4;
-	int intab[K];
-	float distance[50][50];
-	// Tableau avecc toute les distance entre chaque point
-	initialiseDistance(distance, init, 50);
-	P_Cluster clustertab[K];
-
-	printf("%f", distance[0][8]);
-	// genere K valeur aleatoire;
-	numercluster(K, initialnumber, clustertab, init);
-
-	// printf("%f", clustertab[0].C_Cluster[0].Malice);
-	// 	for (int i = 0; i < K; i++)
-
-	// 		creecluster(intab, clustertab, initialnumber, init, K);
-	return 0;
+	float distance[n][n];
+	P_Point points[n];
+	P_Cluster clusters[k];
+	initialisePoints(points, n);
+	initialiseDistance(distance, points, n);
+	initialisecluster(k, n, clusters, points);
+	affecter_cluster_le_plus_proche(k, n, clusters, points);
 }
